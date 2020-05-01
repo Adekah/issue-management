455d142535bf2c79773b88604f9084d816c24c1d
Hüseyin Adeka <huseyin.adeka@etiya.com>
2020-05-01 14:34:47 +0300

    PostgreSQL and ExceptionHandler  was added

---
+++ src/main/java/com/adekah/issuemanagement/advice/ExceptionResponse.java
@@ -0,0 +1,18 @@
+package com.adekah.issuemanagement.advice;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+
+    private Date date;
+    private String message;
+
+}
