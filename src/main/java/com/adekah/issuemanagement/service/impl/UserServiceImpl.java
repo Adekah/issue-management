9b5e2ffcffb1c2ad0b8fb1b4073e5f77c4168424
adeka <adekahuseyin@hotmail.com>
2020-06-27 16:27:13 +0300

    Authentication P2
     AccountController

--- src/main/java/com/adekah/issuemanagement/service/impl/UserServiceImpl.java
+++ src/main/java/com/adekah/issuemanagement/service/impl/UserServiceImpl.java
@@ -1,27 +1,34 @@
 package com.adekah.issuemanagement.service.impl;
 
+import com.adekah.issuemanagement.dto.RegistrationRequest;
 import com.adekah.issuemanagement.dto.UserDto;
 import com.adekah.issuemanagement.entity.User;
 import com.adekah.issuemanagement.repository.UserRepository;
 import com.adekah.issuemanagement.service.UserService;
 import com.adekah.issuemanagement.util.TPage;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
@@ -55,5 +62,20 @@ public class UserServiceImpl implements UserService {
     public UserDto getByUsername(String username) {
         User user = userRepository.findByUsername(username);
         return modelMapper.map(user, UserDto.class);
+    }
+
+    public Boolean register(RegistrationRequest registrationRequest) {
+        try {
+            User user = new User();
+            user.setEmail(registrationRequest.getEmail());
+            user.setNameSurname(registrationRequest.getNameSurname());
+            user.setPassword(bCryptPasswordEncoder.encode(registrationRequest.getPassword()));
+            user.setUsername(registrationRequest.getUsername());
+            userRepository.save(user);
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            log.error("REGISTRATION =>", e);
+            return Boolean.FALSE;
+        }
     }
 }
